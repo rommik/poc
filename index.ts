0d5c0529c9c5c856aee5ed4eb25c1ebf41b5745c
Roman Mikhailov <roman.mikhailov@barco.com>
2020-02-16 00:04:22 -0500

    add simple query, create, display code

---
+++ index.ts
@@ -0,0 +1,22 @@
+import { prisma } from "./generated/prisma-client";
+
+// A `main` function so that we can use async/await
+async function main() {
+  // Lookup Alice
+  const usersCalledAlice = await prisma.users({
+    where: {
+      name: 'Alice',
+    },
+  });
+  console.log(`Lookup returned: ${usersCalledAlice}`);
+  if(!usersCalledAlice){
+  // Create a new user called `Alice`
+  const newUser = await prisma.createUser({ name: "Alice" });
+  console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`);
+  }
+  // Read all users from the database and print them to the console
+  const allUsers = await prisma.users();
+  console.log(allUsers);
+}
+
+main().catch(e => console.error(e));
